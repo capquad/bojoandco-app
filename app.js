f173612ff9628ff14eb2ad9cd54be41a4faaf9a9
peteradeojo <peteradeojo@outlook.com>
2020-11-11 19:16:14 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+	res.render("index", {title: "Hello World"});
+})
+
+app.listen(5000, () => {
+	console.log("Listening on port 5000");
+});
